7b2fd0f3662aeaa0a764d2c68f9b6121ddd911e3
Dongkyu Kim <stleamist@me.com>
2020-04-05 00:15:14 +0900

    GAE 구성 및 Cloud SQL 연결

--- src/test/java/com/hanyang/belieme/demoserver/DemoserverApplicationTests.java
+++ src/test/java/com/hanyang/belieme/demoserver/DemoserverApplicationTests.java
@@ -1,13 +1,13 @@
-package com.hanyang.belieme.demoserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoserverApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.hanyang.belieme.demoserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoserverApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
