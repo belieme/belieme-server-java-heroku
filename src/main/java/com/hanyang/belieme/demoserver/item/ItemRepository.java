6087dd176e797da25e37df75990b3b58fe6d4625
이석환 <huan99@hanmail.net>
2020-08-17 04:55:56 +0000

    (Warning ddl-auto=create)make event table save itemId instead of typeId and itemNum

--- src/main/java/com/hanyang/belieme/demoserver/item/ItemRepository.java
+++ src/main/java/com/hanyang/belieme/demoserver/item/ItemRepository.java
@@ -3,10 +3,8 @@ package com.hanyang.belieme.demoserver.item;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends CrudRepository<Item, Integer> {
-    Optional<Item> findByItemId(int itemId);
     List<Item> findByThingId(int thingId);
     List<Item> findByThingIdAndNum(int thingId, int num);
 }
