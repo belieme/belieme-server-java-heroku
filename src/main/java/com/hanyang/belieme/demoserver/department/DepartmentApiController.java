332a8f6301f0aa85e37e7f8b82b04f590f82bc03
이석환 <huan99@hanmail.net>
2020-08-28 10:51:44 +0000

    major code nullable

--- src/main/java/com/hanyang/belieme/demoserver/department/DepartmentApiController.java
+++ src/main/java/com/hanyang/belieme/demoserver/department/DepartmentApiController.java
@@ -88,6 +88,7 @@ public class DepartmentApiController {
         
         DepartmentDB newDepartmentDB = requestBody.toDepartmentDB();
         newDepartmentDB.setUniversityId(univId);
+        newDepartmentDB.setMajorCodes(null);
         // newDepartmentDB.able(); TODO default는 무엇인가...
         Department output = departmentRepository.save(newDepartmentDB).toDepartment(universityRepository);
         return new ResponseWrapper<Department>(ResponseHeader.OK, output);
